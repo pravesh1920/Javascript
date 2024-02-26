01ae6559dd61d7882f0965cdddb72b892596528d
pravesh1920 <61235397+pravesh1920@users.noreply.github.com>
2024-02-26 19:19:18 +0000

    Datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // Code readability should be high
+
+let name = "Pravesh";
+let age = 18;
+let isLoggedIn = true;
+let state;
+
+/* 
+    number => 2 to power 53
+    bigint
+    string => "", ''
+    boolean => true/false
+    null => standalone value
+    undefind => 
+    symbol => unique
+    
+    
+    object
+*/
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
